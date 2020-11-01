2218e8fcd3e0c574f91215c4551fe2e94ac6237c
amarta <webgraphicshub@gmail.com>
2020-11-02 01:39:54 +0530

    Net Ninja Async Await with Error

--- OCT 2020/Net Ninja Async/script2.js
+++ OCT 2020/Net Ninja Async/script2.js
@@ -1,20 +1,13 @@
 
 const getTodos= async ()=>{
     const response = await fetch('todos/luigi.json');
+    if(response.status !==200){
+        throw new Error('Cannot fetch the Data');
+    }
     const data = await response.json();
     return data;
 };
 
-getTodos().then((data)=>{
-    console.log(data);
-});
-
-
-// fetch('todos/luigi.json').then((response)=>{
-//     console.log('Response: ', response);
-//     return response.json();
-// }).then((data)=>{
-//     console.log(data)
-// }).catch((err)=>{
-//     console.log('Rejected: ', err);
-// })
+getTodos()
+    .then((data)=>{console.log(data);})
+    .catch((err)=>{ console.log("Rejected: ",err.message)})
