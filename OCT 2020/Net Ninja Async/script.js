4376cf81c4f7199c81a1b496d9cdc1b03c2aac26
amarta <webgraphicshub@gmail.com>
2020-11-02 01:13:05 +0530

    Net Ninja Promise

--- OCT 2020/Net Ninja Async/script.js
+++ OCT 2020/Net Ninja Async/script.js
@@ -1,33 +1,59 @@
-const getTodos = (resource, callback)=>{
-    const request = new XMLHttpRequest();
-    request.addEventListener('readystatechange',()=>{
-        // console.log(request, request.readyState);
-        if(request.readyState === 4 && request.status === 200){
-            // console.log(request, request.responseText)
-            const data = JSON.parse(request.responseText)
-            callback(undefined, data);
-        } else if(request.readyState === 4){
-            // console.error("Could't fetch the data");
-            callback('Couldn\'t fetch Data', undefined);
-        }
-    })
-    // https://jsonplaceholder.typicode.com/todos/
-
-    request.open('GET',resource,true);
-    request.send();
-}
+const getTodos = (resource, callback) => {
+return new Promise((resolve, reject)=>{
+      const request = new XMLHttpRequest();
+  request.addEventListener("readystatechange", () => {
+    // console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      // console.log(request, request.responseText)
+      const data = JSON.parse(request.responseText);
+        resolve(data);
+    } else if (request.readyState === 4) {
+      // console.error("Could't fetch the data");
+        reject('Error getting resource')
+    }
+  });
+  // https://jsonplaceholder.typicode.com/todos/
 
 
 
-getTodos('todos/luigi.json',(error, data)=>{
-    console.log(data);
-    getTodos('todos/mario.json',(error, data)=>{
-        console.log(data);
-        getTodos('todos/shaun.json',(error, data)=>{
-            console.log(data); 
-        });
-    });
+  request.open("GET", resource, true);
+  request.send();
 });
 
 
+};
 
+getTodos('todos/luigi.json').then(data=>{
+    console.log('Promise 1 resolved: ',data);
+    return getTodos('todos/mario.json');
+}).then((data)=>{
+    console.log('Promise 2 resolved: ', data);
+    return getTodos('todos/shaun.json');
+}).then((data) =>{
+    console.log('Promise 3 Resolved: ',data);
+}).catch((err) =>{
+    console.log('Promise rejected', err);
+});
+
+//Promise Example
+
+// const getSomething = () => {
+//   return new Promise((resolve, reject) => {
+//     //fetch something
+//     // resolve('some data');
+//     reject();
+//   });
+// };
+
+// getSomething().then((data) =>{
+//     console.log(data);
+// }, (err) =>{
+//     console.log(err);
+// });
+
+
+// getSomething().then((data) =>{
+//     console.log(data);
+// }).catch(err =>{
+//     console.log("Error");
+// });
