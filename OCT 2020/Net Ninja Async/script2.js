3db5d29a5cfd5156317151bee7c6f5f5d8e933b1
amarta <webgraphicshub@gmail.com>
2020-11-02 01:28:29 +0530

    Async Await Basic

--- OCT 2020/Net Ninja Async/script2.js
+++ OCT 2020/Net Ninja Async/script2.js
@@ -1,8 +1,18 @@
-fetch('todos/luigi.json').then((response)=>{
-    console.log('Response: ', response);
-    return response.json();
-}).then((data)=>{
-    console.log(data)
-}).catch((err)=>{
-    console.log('Rejected: ', err);
-})
+
+const getTodos= async ()=>{
+    const response = await fetch('todos/luigi.json');
+    const data = await response.json();
+    console.log(data);
+};
+
+getTodos();
+
+
+// fetch('todos/luigi.json').then((response)=>{
+//     console.log('Response: ', response);
+//     return response.json();
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+//     console.log('Rejected: ', err);
+// })
